3f85fe892bcd232e5f27cdcd8236e7cd466b5403
Ridwan Oladipo <oladiporidwan10@gmail.com>
2024-07-19 21:58:50 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "todo_app.txt"
+
+
+def get_todos(filepath=FILEPATH):  # default parameter
+    """Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):  # non default b4 default
+    """"Write a to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
